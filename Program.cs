badff411da1805ad66e9ee8128345fec2dc517e9
Dayne Game <daynegame024@gmail.com>
2017-08-14 10:11:55 +1200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10012650_task1_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
